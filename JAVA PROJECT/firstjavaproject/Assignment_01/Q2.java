430a6c79eaae4d4110fb8c1131de8f62f38e2257
Rajjoshi123 <100434212+Rajjoshi123@users.noreply.github.com>
2022-05-10 21:14:44 +0530

    Add files via upload

---
+++ JAVA PROJECT/firstjavaproject/Assignment_01/Q2.java
@@ -0,0 +1,24 @@
+package Assignment_01;
+
+public class Q2 {
+public static void main(String[] args)
+{
+	Try obj1=new Try();
+	Try obj2=new Try();
+	Try obj3=new Try();
+	obj1.display();
+	obj2.display();
+	obj3.display();
+	System.out.println("total number of calls for a member function is:" + obj1.count);
+	
+}
+
+}
+
+class Try{
+	static int count=0;
+	void display()
+	{
+		count++;
+	}
+}
